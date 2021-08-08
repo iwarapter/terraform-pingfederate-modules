fc31cebeca783d66467a75de0a9ebad4d6683861
ashhammond <ash.hammond@gmail.com>
2021-08-08 21:13:52 +0100

    adding initial ds test wip

---
+++ test/data_store_rest_api_test.go
@@ -0,0 +1,70 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/iwarapter/pingfederate-sdk-go/pingfederate/client"
+	"github.com/iwarapter/pingfederate-sdk-go/services/dataStores"
+	"github.com/stretchr/testify/assert"
+)
+
+// An example of how to test the simple Terraform module in examples/terraform-basic-example using Terratest.
+func TestDataStoreRestApi(t *testing.T) {
+	// t.Parallel()
+
+	client := dataStores.New(cfg)
+
+	tests := []struct {
+		testName    string
+		name        string
+		baseURL     string
+		apiAttr     map[string]string
+		httpRequest map[string]string
+	}{
+		{
+			testName: "default deployment",
+			name:     "test1",
+			baseURL:  "https://foo.com/api/v1",
+			apiAttr: map[string]string{
+				"foo": "/foo",
+			},
+			httpRequest: map[string]string{
+				"Host": "api.server.com",
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+
+			terraformOptions := &terraform.Options{
+				TerraformDir: "../modules/data_store_rest_api",
+
+				Vars: map[string]interface{}{
+					"name":        tc.name,
+					"baseURL":     tc.baseURL,
+					"apiAttr":     tc.apiAttr,
+					"httpRequest": tc.httpRequest,
+				},
+				NoColor: true,
+			}
+
+			if tc.expectedError {
+				if _, err := terraform.InitAndApplyE(t, terraformOptions); err != nil {
+					assert.Contains(t, err.Error(), tc.errorMessage)
+				}
+			} else {
+				defer terraform.Destroy(t, terraformOptions)
+				terraform.InitAndApply(t, terraformOptions)
+
+				ds, _, err := client.GetDataStores(&dataStores.GetDataStores())
+				
+
+				assert.Nil(t, err)
+				assert.NotNil(t, ds)
+
+				assert.Equal(t, ds.Name, tc.name)
+			}
+		})
+	}
+}
